43841a37e2bf13ee84bcb81202c3d133f5fb78b7
Rakeshsingu <rakeshsinghu2002@gmail.com>
2022-09-25 10:02:24 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,5 @@
+const calculator = require("./calculator");
+const { add, sub } = calculator;
+
+console.log(add(2, 3));
+console.log(sub(2, 3));
---
+++ index.mjs
@@ -0,0 +1,3 @@
+import { add, sub } from "./calculator.mjs";
+console.log(add(3, 6));
+console.log(sub(2, 3));
